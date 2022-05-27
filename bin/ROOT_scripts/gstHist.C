8e55ad4b23e3a0f4a75df6317cc5b5af8da946e4
Noah Everett <noaheverett898@gmail.com>
2022-05-27 13:15:50 -0500

    Fri May 27, 2022, 1:15PM

--- bin/ROOT_scripts/gstHist.C
+++ bin/ROOT_scripts/gstHist.C
@@ -1,17 +1,27 @@
+// Just going to preface this script by saying that it is not efficient at all.
+// There are several postions in here that could be made O(x) but are O(x^2) or O(x^3).
+// Dont use an absorbent amount of files and you wont notice.
+
 using std::string;
 using std::vector;
 using std::map;
 
-const vector< Color_t >     kColors        = { kRed, kBlue, kGreen, kPink };
-const map< string, string > kTitles        = { { "nfn", "Final-State Neutrons" }, { "Q2", "Momentum Transfer" } };
-const map< string, string > kXaxisTitles   = { { "nfn", "Neutron Multiplicity" }, { "Q2", "Momentum Transfer [GeV^2]" } };
-const map< string, string > kYaxisTitles   = { { "nfn", "Events" }, { "Q2", "Counts/GeV^2" } };
-const map< string, string > kBranchVarType = { { "nfn", "int" }, { "Q2", "double" } };
+const vector< Color_t >     kColors         = { kRed, kBlue, kGreen, kYellow, kPink+10, kViolet, kMagenta+4 };
+const map< string, string > kTitles         = { { "nfn", "Final-State Neutrons" }, { "Q2", "Momentum Transfer" } };
+const map< string, string > kXaxisTitles    = { { "nfn", "Neutron Multiplicity" }, { "Q2", "Momentum Transfer [GeV^2]" } };
+const map< string, string > kYaxisTitles    = { { "nfn", "Events" }, { "Q2", "Counts/GeV^2" } };
+const map< string, string > kBranchVarTypes = { { "nfn", "int" }, { "Q2", "double" } };
+const map< string, int    > kPdgCodes       = { { "H1"  , 1000010010 }, { "C12" , 1000060120 },
+                                                { "N14" , 1000070140 }, { "O16" , 1000080160 },
+                                                { "Na23", 1000110230 }, { "Mg24", 1000120240 },
+                                                { "Al27", 1000130270 }, { "Si28", 1000140280 },
+                                                { "Ar40", 1000180400 }, { "K39" , 1000190390 },
+                                                { "Ca40", 1000200400 }, { "Fe56", 1000260560 } };
 
-int makeHist( const vector< string >& t_files, const string& t_branch, const string& t_options );
+int makeHist( const vector< string >& t_files, const string& t_branch, const string& t_target, const string& t_options );
 int getDigits( const int& t_int );
 
-void gstHist( vector< string > t_files, string t_branch, string t_options ) 
+void gstHist( vector< string > t_files, string t_branch, string t_target, string t_options ) 
 {
     for( char option : t_options )
         if( option == 'H' | option == 'h' ) {
@@ -33,13 +43,15 @@ void gstHist( vector< string > t_files, string t_branch, string t_options )
             return 1;
         }
 
-    if( makeHist( t_files, t_branch, t_options ) != 0 ) return 1;
+    if( !kPdgCodes.count( t_target ) ) cerr << "Invalid t_target. Ex Ar40 | O16" << endl;
+
+    if( makeHist( t_files, t_branch, t_target, t_options ) != 0 ) return 1;
 
     return 0;
     
 }
 
-int makeHist( const vector< string >& t_files, const string& t_branch, const string& t_options )
+int makeHist( const vector< string >& t_files, const string& t_branch, const string& t_target, const string& t_options )
 {
     bool oPercent = false;
     bool oFill    = false;
@@ -55,17 +67,18 @@ int makeHist( const vector< string >& t_files, const string& t_branch, const str
     TFile* file;
     TTree* tree;
     TH1F * hists[ t_files.size() ];
-    // TH1F  * hists;
 
-    string type = kBranchVarType.at( t_branch );
+    string type = kBranchVarTypes.at( t_branch );
     string opt = "HIST";
 
     int    size;
     double cur_double;
     int    cur_int;
+    int    cur_tgt;
     double maxes[ t_files.size() ];
     double max;
     vector< double > branches[ t_files.size() ];
+    bool error = false;
 
     for( int i = 0; i < t_files.size(); i++ ) {
         cout << "Retrieving information from file: `" << t_files[ i ] << "`, branch: `" << t_branch << "`." << endl;
@@ -77,18 +90,28 @@ int makeHist( const vector< string >& t_files, const string& t_branch, const str
              if( type == "int"    ) tree->SetBranchAddress( t_branch.c_str(), &cur_int    );
         else if( type == "double" ) tree->SetBranchAddress( t_branch.c_str(), &cur_double );
         else                        tree->SetBranchAddress( t_branch.c_str(), &cur_double );
+        tree->SetBranchAddress( "tgt", &cur_tgt );
 
         for( Int_t j = 0; j < size; j++ ) {
             tree->GetEntry( j );
-                 if( type == "int" )    branches[ i ].push_back( cur_int    );
-            else if( type == "double" ) branches[ i ].push_back( cur_double );
-            else                        branches[ i ].push_back( cur_double );
+            if( t_target != 0 && cur_tgt == kPdgCodes.at( t_target ) ) {
+                     if( type == "int" )    branches[ i ].push_back( cur_int    );
+                else if( type == "double" ) branches[ i ].push_back( cur_double );
+                else                        branches[ i ].push_back( cur_double );
 
-                 if( type == "int" )    { if( cur_int    > maxes[ i ] ) maxes[ i ] = cur_int   ; }
-            else if( type == "double" ) { if( cur_double > maxes[ i ] ) maxes[ i ] = cur_double; }
-            else                        { if( cur_double > maxes[ i ] ) maxes[ i ] = cur_double; }
+                     if( type == "int" )    { if( cur_int    > maxes[ i ] ) maxes[ i ] = cur_int   ; }
+                else if( type == "double" ) { if( cur_double > maxes[ i ] ) maxes[ i ] = cur_double; }
+                else                        { if( cur_double > maxes[ i ] ) maxes[ i ] = cur_double; }
+            }
         }
     }
+    
+    for( int i = 0; i < t_files.size(); i++ ) 
+        if( branches[ i ].size() == 0 ) {
+            cerr << "branches[ " << i << " ] has size = 0. Change target or input files." << endl;
+            error = true;
+        }
+    if( error ) return 1;
 
     cout << "Finding maximum value from all files." << endl;
     for( int i = 0; i < t_files.size(); i++ )
@@ -97,14 +120,17 @@ int makeHist( const vector< string >& t_files, const string& t_branch, const str
     for( int i = 0; i < t_files.size(); i++ ) {
         cout << "Making histogram for file: `" << t_files[ i ] << "`, branch: `" << t_branch << "`." << endl;
 
+        string title = kTitles.at( t_branch );
+        if( t_target != 0 ) title += " (Events in " + t_target + ")";
+    
         if( type == "int" )
-            hists[ i ] = new TH1F( t_files[ i ].c_str(), kTitles.at( t_branch ).c_str(), max + 1, 0, max );
+            hists[ i ] = new TH1F( t_files[ i ].c_str(), title.c_str(), max, 0, max );
         else if( type == "double" )
-            hists[ i ] = new TH1F( t_files[ i ].c_str(), kTitles.at( t_branch ).c_str(), max * 20, 0, max );
+            hists[ i ] = new TH1F( t_files[ i ].c_str(), title.c_str(), max * 20, 0, max );
         else
-            hists[ i ] = new TH1F( t_files[ i ].c_str(), kTitles.at( t_branch ).c_str(), max * 20, 0, max );
+            hists[ i ] = new TH1F( t_files[ i ].c_str(), title.c_str(), max * 20, 0, max );
 
-        for( int j = 0; j < size; j++ )
+        for( int j = 0; j < branches[ i ].size(); j++ )
             hists[ i ]->Fill( branches[ i ][ j ] );
 
         if( oFill ) { 
@@ -125,19 +151,25 @@ int makeHist( const vector< string >& t_files, const string& t_branch, const str
         hists[ i ]->GetXaxis()->SetTitleOffset( 1.15 );
     }
 
+    cout << "Making and filling legend." << endl;
     TLegend* legend = new TLegend();
     for( int i = 0; i < t_files.size(); i++ )
         legend->AddEntry( hists[ i ], t_files[ i ].c_str(), "l" );
 
     // Literally insertion sorting histograms. lol
+    cout << "Sorting histograms." << endl;
     for( int i = 1; t_files.size() > 1 && i < t_files.size(); i++ ) {
-        if( hists[ i - 1 ]->GetBinContent( hists[ i - 1 ]->GetMaximumBin() ) < 
-            hists[ i ]->GetBinContent( hists[ i ]->GetMaximumBin() ) ) {
-            TH1F* temp = hists[ i - 1 ];
-            hists[ i - 1 ] = hists[ i ];
-            hists[ i ] = temp;
+        for( int j = 0 ; i - j - 1 >= 0 && hists[ i - j - 1 ]->GetBinContent( hists[ i - j - 1 ]->GetMaximumBin() ) < 
+                                           hists[ i - j ]->GetBinContent( hists[ i - j ]->GetMaximumBin() ); j++ ) {
+            cout << "    Swapping `hists[ " << i - j - 1 << " ]` and `hists[ " << i - j << " ]`." << endl;
+            TH1F* temp = hists[ i - j - 1 ];
+            hists[ i - j - 1 ] = hists[ i - j ];
+            hists[ i - j ] = temp;
         }
     }
+    cout << "Sorted histograms:" << endl;
+    for( int i = 0; i < t_files.size(); i++ )
+        cout << "    `hists[ " << i << " ]` --> Max bin size = " << hists[ i ]->GetBinContent( hists[ i ]->GetMaximumBin() ) << "." << endl;
 
         for( int i = 0; i < t_files.size(); i++ )
             hists[ i ]->GetYaxis()->SetTitleOffset( 0.7 + 
@@ -162,7 +194,7 @@ int getDigits( const int& t_int ) {
         return 1;
 
     int digits = 1;
-    for( ; integer / 10 > 0; integer /= 10 ) ++digits;
+    for( ; integer / 10 > 0; integer /= 10, ++digits ) {}
 
     return digits;
 }
