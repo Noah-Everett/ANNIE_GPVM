d89fca26e4467b54b07c43f1206d17f1f73abe24
Noah Everett <noaheverett898@gmail.com>
2022-05-30 16:01:16 -0500

    Mon May 30, 2022, 4:01PM

--- bin/setup_shortcuts.sh
+++ bin/setup_shortcuts.sh
@@ -1,5 +1,5 @@
 # /annie/app/users/
-export ANNIEUSERS=/annie/app/users
+export USERS=/annie/app/users
 export NE=$ANNIEUSERS/neverett
 export RH=$ANNIEUSERS/rhatcher
 export JM=$ANNIEUSERS/jminock
@@ -7,18 +7,18 @@ export MO=$ANNIEUSERS/moflaher
 export MA=$ANNIEUSERS/mascenci
 export FL=$ANNIEUSERS/flemmons
 
-# /pnfs/annie/scratch/users/
-export PNFSUSERS=/pnfs/annie/scratch/users
-export PNE=$PNFSUSERS/neverett
-export PG=$PNE/genie_output
-
-# personal
+# /annie/app/users/neverett/
 export B=$NE/bin
 export R=$NE/runs
-export W=$NE/WCSim
 export G=$NE/geometry
 export T=$NE/ToolAnalysis
-export GR=$NE/grid
-export F=$NE/flux
 export C=$NE/config
-export BA=$NE/backups
+export GR=$NE/grid
+export BA=$NE/backupsV
+
+# /pnfs/annie/scratch/users/
+export PUSERS=/pnfs/annie/scratch/users
+
+# /pnfs/annie/scratch/users/neverett/
+export PNE=$PNFSUSERS/neverett
+export PG=$PNE/genie_output
